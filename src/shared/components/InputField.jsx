d32c1790dca61fe42fcc1fa5c66cfd95816ee1ff
seongjongju <sjj990480@gmail.com>
2025-09-22 23:42:48 +0900

    회원가입 폼 작업 중 이메일인증 인풋 수정 및 회원가입 요청 수정작업 예정

--- src/shared/components/InputField.jsx
+++ src/shared/components/InputField.jsx
@@ -4,33 +4,43 @@ import inputClear from '../../assets/images/input_clear.svg';
 
 const InputField = ({ label, type, onChange, value, htmlFor, inputReset }) => {
     const [isInputFocus, setisInputFocus] = useState(false);
+    const [isInputFocusLabel, setisInputFocusLabel] = useState(false);
         
     const onBlur = () => {
+        setisInputFocus(false);
+
         if(value.trim() !== '') {
-            setisInputFocus(true);
+            setisInputFocusLabel(true);
         } else {
-            setisInputFocus(false);
+            setisInputFocusLabel(false);
         }
     };
 
     return (
         <div className={isInputFocus ? inputFieldStyleActive : inputFieldStyle}>
-            <label className={isInputFocus ?  labelStyleActive : labelStyle} htmlFor={htmlFor}>{label}</label>
+            <label className={isInputFocusLabel ?  labelStyleActive : labelStyle} htmlFor={htmlFor}>{label}</label>
             <input 
                 id={htmlFor}
                 className='w-[calc(100%-20px)] h-[35px] outline-none border-0 block absolute bottom-[0] text-[16px] text-[#1F2937]'
                 type={type}
                 onChange={onChange}
                 value={value}
-                onFocus={() => setisInputFocus(true)}
+                onFocus={() => {
+                    setisInputFocus(true)
+                    setisInputFocusLabel(true)
+                }}
                 onBlur={onBlur}
             />
             <button 
                 className='absolute top-[50%] right-[20px] translate-y-[-50%]'
                 type='button'
-                onClick={inputReset}
+                onClick={() => {
+                    inputReset()
+                    setisInputFocus(false)
+                    setisInputFocusLabel(false)
+                }}
             >
-                {isInputFocus && <img src={inputClear} alt='모두 지우기' />}
+                {isInputFocusLabel && <img src={inputClear} alt='모두 지우기' />}
             </button>
         </div>
     );
