887ea024128bd98cb3863c50bbd1cb1a859bb93b
donghyeon <enthchal@gmail.com>
2025-09-25 19:11:43 +0900

    로그인 API 수정

--- src/features/api/authApi.js
+++ src/features/api/authApi.js
@@ -1,50 +1,45 @@
-import axios from 'axios';
+import axios from "axios";
 //이메일 인증번호 발송
-export const postEmail =  async ({ email }) => {
-    const res = await axios.post('https://api.sawonz.world/email/send-code',
-        {
-            email
-        },
-    );
+export const postEmail = async ({ email }) => {
+  const res = await axios.post("https://api.sawonz.world/email/send-code", {
+    email,
+  });
 
-    return res;
+  return res;
 };
 
 //인증번호 확인
 export const citationCheck = async ({ email, verificationCode }) => {
-    const res = await axios.post('https://api.sawonz.world/email/check-code', 
-        {
-            email,
-            verificationCode
-        },
-    );
+  const res = await axios.post("https://api.sawonz.world/email/check-code", {
+    email,
+    verificationCode,
+  });
 
-    return res;
+  return res;
 };
 
 //회원가입
-export const fetchSignUp = async ({userName, phone, email, password}) => {
-    const res = await axios.post('https://api.sawonz.world/users/signup',
-        {
-            userName,
-            phone,
-            email,
-            password
-        },
-    );
+export const fetchSignUp = async ({ userName, phone, email, password }) => {
+  const res = await axios.post("https://api.sawonz.world/users/signup", {
+    userName,
+    phone,
+    email,
+    password,
+  });
 
-    return res;
+  return res;
 };
 
 //로그인
-export const fetchLogin = async ({email, password}) => {
-    const res = await axios.post('...API주소...',
-        {
-            email,
-            password
-        },
-        {withCredentials: true}
-    );
+export const fetchLogin = async ({ email, password }) => {
+  const res = await axios.post(
+    "https://api.sawonz.world/auth/login",
+    {
+      email,
+      password,
+    },
+    { withCredentials: true }
+  );
 
-    return res;
+  return res;
 };
