d1b1efff7102ba4490deac77e22a0bc7d2ab9ed9
seongjongju <sjj990480@gmail.com>
2025-09-26 16:42:01 +0900

    zustand 미들웨어 추가

--- src/store/useUserStore.jsx
+++ src/store/useUserStore.jsx
@@ -1,22 +1,27 @@
 import { create } from "zustand";
 import { userMe } from "../features/api/userApi";
+import { persist } from "zustand/middleware";
 
-export const useAuth = create((set) => ({
-    user: null,
-    error: '',
-    isLogged: false,
-    isLoading: false,
+export const useAuth = create(
+    persist(
+        (set) => ({
+            user: null,
+            error: '',
+            isLogged: false,
+            isLoading: false,
 
-    userInfo: async () => {
-        try {
-            set({ isLoading: true })
+            userInfo: async () => {
+                try {
+                    set({ isLoading: true })
 
-            const res = await userMe();
-            console.log("userMe 응답:", res.data);
+                    const res = await userMe();
+                    console.log("userMe 응답:", res.data);
 
-            set({ user: res.data.data, error: null, isLogged: true, isLoading: false });
-        } catch(err) {
-            set({ user: null, error: `${err} 로그인 정보 없음`, isLogged: false, isLoading: false });
-        }
-    },
-}));
+                    set({ user: res.data.data, error: null, isLogged: true, isLoading: false });
+                } catch(err) {
+                    set({ user: null, error: `${err} 로그인 정보 없음`, isLogged: false, isLoading: false });
+                }
+            },
+        })
+    )
+);
