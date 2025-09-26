7316c8b27c2757ddb69251518bd9991bd1423af5
seongjongju <sjj990480@gmail.com>
2025-09-26 14:52:39 +0900

    로그이 유지 재 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
     return (
         <main className={mainLayout}>
             <div className={mainContents}>
-                {user.userName}
+                {user.data.userName}
             </div>
         </main>
     );
