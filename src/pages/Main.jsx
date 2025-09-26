45846c07c68fc80b32e05e3ef7be90dfe1c12dd6
seongjongju <sjj990480@gmail.com>
2025-09-26 15:59:14 +0900

    제발

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,9 +3,17 @@ import { mainContents, mainLayout } from '../shared/styles/commonTailwind';
 import { useAuth } from '../store/useUserStore';
 
 const Main = () => {
-    const {user, isLogged, error} = useAuth();
-    console.log("Main user 상태:", user, isLogged, error);
+    const {user, isLogged, error, isLoading} = useAuth();
+    console.log("Main user 상태:", user, isLogged, error, isLoading);
     
+    if(isLoading) {
+        return (
+            <main className={mainLayout}>
+                <p>로딩 중...</p>
+            </main>
+        )
+    }
+
     return (
         <main className={mainLayout}>
             <div className={mainContents}>
--- src/store/useUserStore.jsx
+++ src/store/useUserStore.jsx
@@ -3,17 +3,20 @@ import { userMe } from "../features/api/userApi";
 
 export const useAuth = create((set) => ({
     user: null,
-    error: null,
+    error: '',
     isLogged: false,
+    isLoading: false,
 
     userInfo: async () => {
         try {
+            set({ isLoading: true })
+
             const res = await userMe();
             console.log("userMe 응답:", res.data);
 
-            set({ user: res.data.data, error: null, isLogged: true });
+            set({ user: res.data.data, error: null, isLogged: true, isLoading: false });
         } catch(err) {
-            set({ user: null, error: err.response.data.message, isLogged: false });
+            set({ user: null, error: `${err} 로그인 정보 없음`, isLogged: false, isLoading: false });
         }
     },
 }));
